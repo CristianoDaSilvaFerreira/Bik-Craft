6f365d90bd9bbb1e104228c0ddf1be0d3c7fe439
CRISTIANO DA SILVA FERREIRA <68359459+CristianoDaSilvaFerreira@users.noreply.github.com>
2021-06-14 20:27:16 -0300

    :package: Criando as animações e linkando plugins JS e Modernizr

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(function () {
+    $(".rslides").responsiveSlides();
+});
+
+$(".rslides").responsiveSlides({
+    auto: true,             // Boolean: Animate automatically, true or false
+    speed: 500,            // Integer: Speed of the transition, in milliseconds
+    timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+});
+
+$(".rslides_portfolio").responsiveSlides({
+    auto: true,             // Boolean: Animate automatically, true or false
+    speed: 500,            // Integer: Speed of the transition, in milliseconds
+    timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+    pager: true,           // Boolean: Show pager, true or false
+});
+
+Visibility.onVisible(function () {
+    //   Animações Home
+    setTimeout(function () {
+        $(".introducao h1").addClass("animated fadeInDown");
+    }, 400);
+
+    setTimeout(function () {
+        $(".introducao blockquote").addClass("animated fadeInDown");
+    }, 800);
+
+    setTimeout(function () {
+        $(".introducao .btn").addClass("animated fadeInDown");
+    }, 1200);
+
+    setTimeout(function () {
+        $(".animar").addClass("animated fadeInDown");
+    }, 1600);
+
+    //   Animações páginas internas
+    setTimeout(function () {
+        $(".introducao-interna h1").addClass("animated fadeInDown");
+    }, 400);
+
+    setTimeout(function () {
+        $(".introducao-interna p").addClass("animated fadeInDown");
+    }, 800);
+
+    setTimeout(function () {
+        $(".animar-interno").addClass("animated fadeInDown");
+    }, 1200);
+
+});
